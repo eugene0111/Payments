ebfa6216e9cf97a14d6a59dd0bf5eaebc6de8617
eugene0111 <eugenesam1105@gmail.com>
2026-03-05 23:42:13 +0530

    test

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -58,7 +58,8 @@ main()
     await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+  
